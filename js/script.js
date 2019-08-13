8680c5f178582d1da06e1aaa205d88a1ea40709f
AsupDom <asup.domochay@yandex.by>
2019-08-13 23:32:39 +0300

    Add new changes

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@
 let money = 0,
 	time;
 
-money = +prompt("Ваш бюджет на месяц?", "");
-time = prompt("Введите дату в формате YYYY-MM-DD", "2019-08-06");
+function start() {
+	money = +prompt("Ваш бюджет на месяц?", "");
+	time = prompt("Введите дату в формате YYYY-MM-DD", "2019-08-06");
+
+	while (isNaN(money) || money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", "");
+	}
+}
+
+start();
 
 let appData = {
 	budjet: money,
@@ -12,23 +20,38 @@ let appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
+	savings: true
 };
 
-let i = 0;
+function chooseExpenses() {
+	let i = 0;
 
-while (i < 2) {
-	let r = prompt("Введите обязательную статью расходов в этом месяце", ""),
-		a = +prompt("Во сколько обойдется?", ""); 
-	
-	if ( (typeof(r) === 'string') && (typeof(r) != null) && (typeof(a) != null) && (r != '') & (a != 0) ) {
-		appData.expenses[r] = a;
-		i++;
-	} else {
-		continue;
+	while (i < 2) {
+		let r = prompt("Введите обязательную статью расходов в этом месяце", ""),
+			a = +prompt("Во сколько обойдется?", ""); 
+		
+		if ( (typeof(r) === 'string') && (typeof(r) != null) && (typeof(a) != null) && (r != '') & (a != 0) ) {
+			appData.expenses[r] = a;
+			i++;
+		} else {
+			continue;
+		}
 	}
 }
 
-appData.budjetPerDay = appData.budjet / 30;
+chooseExpenses();
+
+appData.budjetPerDay = (appData.budjet / 30).toFixed(1);
+
+function checkSavings() {
+	if (appData.savings) {
+		let save = +prompt("Какова сумма накоплений?"),
+			percent = +prompt("Каков просцент? %");
+
+		appData.monthIncome = +(save * percent / 100 / 12);
+	}
+}
+
+checkSavings();
 
 console.log(appData);
